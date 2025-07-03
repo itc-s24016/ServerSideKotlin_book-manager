1563e53dd4dd620c2844b37e5156231b6a34a4cb
itc-s24016 <s24016@std.it-college.ac.jp>
2025-07-03 15:59:58 +0900

    変更あり

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "2.1.21"
+    kotlin("plugin.spring") version "2.1.21"
+    kotlin("plugin.serialization") version "2.1.21"
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.qqviaja.gradle.MybatisGenerator") version "2.5"
 }
 
 group = "jp.ac.it_college.std.s24016.kotlin"
@@ -22,6 +24,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.0")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.4")
+    implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.5.2")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
@@ -36,4 +42,19 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+mybatisGenerator{
+    verbose = true
+    configFile = "${projectDir}/src/main/resources/generatorConfig.xml"
+    mybatisProperties.putAll(mapOf(
+        "driverClass" to "org.mariadb.jdbc.Driver",
+        "connectionURL" to "jdbc:mariadb://127.0.0.1:3306/book_manager",
+        "username" to "spring",
+        "password" to "boot"
+    ))
+    dependencies {
+        mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.1")
+        mybatisGenerator("org.mariadb.jdbc:mariadb-java-client")
+    }
 }
