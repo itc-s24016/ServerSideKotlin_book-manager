e09ee699f14204e1264b273f5cfd1ef2e0fc1fb0
itc-s24016 <s24016@std.it-college.ac.jp>
2025-07-09 16:08:54 +0900

    新しいテーブルを追加

--- src/main/kotlin/jp/ac/it_college/std/s24016/kotlin/book/manager/infrastructure/database/record/BookWithRental.kt
+++ src/main/kotlin/jp/ac/it_college/std/s24016/kotlin/book/manager/infrastructure/database/record/BookWithRental.kt
@@ -1,3 +1,14 @@
 package jp.ac.it_college.std.s24016.kotlin.book.manager.infrastructure.database.record
 
-data class BookWithRental()
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class BookWithRental(
+    var id: Long? = null,
+    var title: String? = null,
+    var author: String? = null,
+    var releaseDate: LocalDate? = null,
+    var userId: Long? = null,
+    var rentalDatetime: LocalDateTime? = null,
+    var returnDeadline: LocalDateTime? = null,
+)
