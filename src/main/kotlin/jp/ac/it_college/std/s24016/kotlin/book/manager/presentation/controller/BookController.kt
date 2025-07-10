6bcd5ed972a53c1742c20064947a8785b5274151
itc-s24016 <s24016@std.it-college.ac.jp>
2025-07-10 16:12:16 +0900

    BookInfoのリストを返す

---
+++ src/main/kotlin/jp/ac/it_college/std/s24016/kotlin/book/manager/presentation/controller/BookController.kt
@@ -0,0 +1,23 @@
+package jp.ac.it_college.std.s24016.kotlin.book.manager.presentation.controller
+
+import jp.ac.it_college.std.s24016.kotlin.book.manager.application.service.BookService
+import jp.ac.it_college.std.s24016.kotlin.book.manager.presentation.from.BookInfo
+import jp.ac.it_college.std.s24016.kotlin.book.manager.presentation.from.GetBookListResponse
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/book")
+//セキュリティ機能を導入するときに必要
+@CrossOrigin
+class BookController (
+    private val bookService: BookService
+){
+    @GetMapping("/list") //BookInfoのリストを返す
+    fun getList(): GetBookListResponse {
+        val bookList = bookService.getList().map(::BookInfo)
+        return GetBookListResponse(bookList)
+    }
+}
