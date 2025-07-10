eeb5eb3e7ad41d9516d8d64eddcfdf0a49d5fafc
itc-s24016 <s24016@std.it-college.ac.jp>
2025-07-10 16:03:02 +0900

    データを一覧表示用に提供するクラス

--- src/main/kotlin/jp/ac/it_college/std/s24016/kotlin/book/manager/application/service/BookService.kt
+++ src/main/kotlin/jp/ac/it_college/std/s24016/kotlin/book/manager/application/service/BookService.kt
@@ -1,4 +1,14 @@
 package jp.ac.it_college.std.s24016.kotlin.book.manager.application.service
-
-class BookService {
+//データベースからデータを取得、一覧表示用に提供するクラス
+import jp.ac.it_college.std.s24016.kotlin.book.manager.domain.model.BookWithRental
+import jp.ac.it_college.std.s24016.kotlin.book.manager.domain.repository.BookRepository
+import org.springframework.stereotype.Service
+//このクラスがビジネスロジックをを扱う「サービス層」のコンポーネントであることを指す
+@Service
+class BookService (
+    private val bookRepository: BookRepository
+){
+    fun getList(): List<BookWithRental> {
+        return bookRepository.findAllWithRental()
+    }
 }
