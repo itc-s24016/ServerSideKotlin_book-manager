ce49f847ca112137945ac571e0218d861781d568
itc-s24016 <s24016@std.it-college.ac.jp>
2025-07-10 15:58:21 +0900

    関数を定義したインターフェース

--- src/main/kotlin/jp/ac/it_college/std/s24016/kotlin/book/manager/domain/repository/BookRepository.kt
+++ src/main/kotlin/jp/ac/it_college/std/s24016/kotlin/book/manager/domain/repository/BookRepository.kt
@@ -1,4 +1,8 @@
 package jp.ac.it_college.std.s24016.kotlin.book.manager.domain.repository
 
-class BookRepository {
+import jp.ac.it_college.std.s24016.kotlin.book.manager.domain.model.BookWithRental
+
+//　関数を定義したインターフェース
+interface BookRepository {
+    fun findAllWithRental(): List<BookWithRental>
 }
