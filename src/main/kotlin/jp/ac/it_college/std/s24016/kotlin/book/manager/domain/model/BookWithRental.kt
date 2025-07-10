2276d23036742c197c63c605a29f199d84ff490a
itc-s24016 <s24016@std.it-college.ac.jp>
2025-07-10 16:03:28 +0900

    本のレンタル状況を表すデータクラス

--- src/main/kotlin/jp/ac/it_college/std/s24016/kotlin/book/manager/domain/model/BookWithRental.kt
+++ src/main/kotlin/jp/ac/it_college/std/s24016/kotlin/book/manager/domain/model/BookWithRental.kt
@@ -1,3 +1,12 @@
 package jp.ac.it_college.std.s24016.kotlin.book.manager.domain.model
 
-data class BookWithRental()
+//本のレンタル状況を表すデータクラス
+data class BookWithRental(
+    //BookクラスとRentalクラスの情報を保有、照合する
+    val book: Book,
+    val rental: Rental?, //レンタルされていない場合もあるので許容型
+) {
+    //このデータクラス特有のメソッド（レンタルされているか判断）
+    val isRental: Boolean
+        get() = rental != null //レンタルが Null でなければ True
+}
